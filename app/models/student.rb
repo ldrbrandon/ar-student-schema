9edb912e810121da5b46bd26509c5e9d13df8248
dbc-apprentice <dbc-apprentice@dbc.com>
2013-10-02 15:52:07 -0700

    .

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,17 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  
+  attr_reader :first_name, :last_name, :age
+
+  def initialize(args = {})
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @age = args[:age]
+  end
+
+  def name
+    first_name + " " + last_name
+  end
 end
+
