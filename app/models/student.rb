56f54a01ff2ab4c3f5f9d7d3d61fe42aa7d4d530
dbc-apprentice <dbc-apprentice@dbc.com>
2013-10-02 17:53:50 -0700

    .

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,13 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 
+  validates :age, numericality: { only_integer: true,
+    greater_than_or_equal_to: 5}
+  validates :email, format: { with: /.+@\w{1,}.\w{2,}/,
+    message: "Must have at least 2 characters before between and after the @ sign and ." }
+  validates :email, uniqueness: true
+  validate :phone_num_must_be_atleast_ten_digits
+
   def name
     self.first_name + " " + self.last_name
   end
@@ -10,4 +17,15 @@ class Student < ActiveRecord::Base
     (Date.today - self.birthday).to_i / 365
   end
 
+  def phone_num_must_be_atleast_ten_digits
+    if self.phone.gsub(/\D/,"").length < 10
+      errors.add(:phone, "Phone number can't be less than 10 digits")
+    end
+  end
+
+
 end
+
+# at least 10 digits
+
+
